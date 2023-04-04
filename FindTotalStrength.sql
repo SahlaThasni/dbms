4140b49358df85a57987e7b3f237ad712fa1688e
SahlaThasni <116860950+SahlaThasni@users.noreply.github.com>
2023-04-04 14:13:50 +0530

    Add files via upload

---
+++ FindTotalStrength.sql
@@ -0,0 +1,19 @@
+create database abc;
+use abc;
+create table section(s_id int,s_name varchar(20),strength int);
+insert into section values(1,"computer science",20);
+insert into section values(2,"portal",45);
+insert into section values(3,"geekforgeeks",60);
+DELIMITER //
+create function totalStrength()
+returns int
+deterministic
+begin
+declare total int;
+set total=0;
+select sum(strength) into total from section;
+return total;
+end;
+// 
+DELIMITER;
+select totalStrength();
